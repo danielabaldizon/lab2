a69d97a9484660381b22b03eab8e6f1b7798f767
Daniela Baldizón <bal17005@uvg.edu.gt>
2020-01-31 22:02:04 -0600

    inicio

---
+++ Laboratorio2.X/lab2.c
@@ -0,0 +1,27 @@
+/* 
+ * File:   lab2.c
+ * Author: Daniela Baldizon
+ *
+ * Created on 29 de enero de 2020, 04:13 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+//jfkdsahfklsdhlkfhjaksdhfkjsahdfkjahskjfdsa
+
+
+
+//prebaflkjsdlfjasldjflks
+
+
+
+int main(int argc, char** argv) {
+
+    
+    return (EXIT_SUCCESS);
+}
+
